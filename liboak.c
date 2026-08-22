595a51cd6f854b0e276f9dcf5d3a176e5b57b10f
cangcang <cangcang_code@icloud.com>
2026-08-22 20:26:33 +0800

    add OPL license headers to source files

--- liboak.c
+++ liboak.c
@@ -1,4 +1,18 @@
 /*
+ * Copyright (C) 2026 OPENOS-dev
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the OPENOS-PROJECT-LICENSE (OPL) v1.2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OPL for more details.
+ *
+ * You should have received a copy of the OPL along with this program.
+ * If not, see <https://github.com/OPENOS-dev/OPL>.
+ */
+
+/*
  * liboak.c — OPENOS Settings API 实现 (见 liboak.h)
  * 依赖 libsodium (SHA-256) + POSIX (unix socket / fork/exec)。
  */
